1cfc063f68b54d9b98697e44717ba484cc9d1681
Korzhov <honeyiamgood@mail.ru>
2019-09-15 12:09:47 +0300

    Initial commit

---
+++ WebAPIAuth/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAPIAuth.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
